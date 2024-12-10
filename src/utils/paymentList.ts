bd87a9ce94c1b5ef3590603e962f2c77ce933fcc
HT-35 <huyfa352002@gmail.com>
2024-12-10 21:04:43 +0700

    update url payment

--- src/utils/paymentList.ts
+++ src/utils/paymentList.ts
@@ -4,5 +4,6 @@ export const paymentList = {
   vnp_Url: "https://sandbox.vnpayment.vn/paymentv2/vpcpay.html",
   vnp_Api: "https://sandbox.vnpayment.vn/merchant_webapi/api/transaction",
   //vnp_ReturnUrl: "http://localhost:3232/order/vnpay_return",
-  vnp_ReturnUrl: "http://localhost:3000/bill/",
+  //vnp_ReturnUrl: "http://localhost:3000/bill/", //localhost
+  vnp_ReturnUrl: "http://http://14.225.254.209:3000/bill/", //cloud
 };
