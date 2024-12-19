dfcd6898479871b55bc65f6cf2eaa85c8c1c9cfa
HT-35 <huyfa352002@gmail.com>
2024-12-20 03:15:00 +0700

    update return url payment

--- src/modules/order/dto/update-order-model.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from "@nestjs/mapped-types";
-import { CreateOrderModelDto } from "./create-order-model.dto";
-
-export class UpdateOrderModelDto extends PartialType(CreateOrderModelDto) {}
--- src/modules/order/order-model.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from "@nestjs/common";
-import { OrderModelService } from "./order-model.service";
-import { OrderModelController } from "./order-model.controller";
-
-@Module({
-  controllers: [OrderModelController],
-  providers: [OrderModelService],
-})
-export class OrderModelModule {}
